a64bbb739bc271a9a7994f4b47fa4fee9efb0869
shahid <shahdbest@gmail.com>
2022-08-18 14:58:20 +0530

    a

---
+++ src/main/java/com/stackroute/collections/UniqueNumbersCounter.java
@@ -0,0 +1,8 @@
+package com.stackroute.collections;
+
+public class UniqueNumbersCounter {
+    //write logic to find maximum unique numbers count from given array in the sub array of certain length
+    public String findUniqueNumbersCount(int[] inputArray, int subArrayLength) {
+       return null;
+    }
+}
